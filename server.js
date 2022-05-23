6d837245a31d24604295e7aa98051e68b28643f0
jrosent8 <86493608+jrosent@users.noreply.github.com>
2022-05-23 00:18:57 -0700

    add node packages and move html to its own folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
-var ROOT_DIR = "./";
+var ROOT_DIR = "./html";
 http.createServer(function (req, res) {
     var urlObj = url.parse(req.url, true, false);
     if (req.method == "GET") {
